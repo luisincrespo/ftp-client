36817fac6e93dce901fe4b76f293aa063607f46b
Luis Crespo <luiscrisf@gmail.com>
2017-11-09 19:56:55 -0500

    Better connect messages.

--- client.py
+++ client.py
@@ -7,18 +7,22 @@ class FtpClient(object):
         self._socket = socket()
 
     def _reset_socket(self):
-        print('Disconnecting from {}'
+        print('Dropping existing connection to {}'
               .format(self._socket.getpeername()))
         self._socket.close()
         self._socket = socket()
 
-    def connect(self, host='localhost'):
-        print 'Connecting to {}'.format(host)
+    def connect(self, host):
+        if host is None:
+            host = 'localhost'
+
         try:
+            print 'Connecting to {}'.format(host)
             self._socket.connect((host, 21))
         except error as (number, string):
             if number == errno.EISCONN:
                 self._reset_socket()
                 self._socket.connect((host, 21))
+
         data = self._socket.recv(4096)
         return repr(data)
