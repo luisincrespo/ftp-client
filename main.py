d19767534e2683fd87fafad5fb1d2eeef4d80d68
Luis Crespo <luiscrisf@gmail.com>
2017-12-08 11:27:38 -0500

    Add --debug param help message.

--- main.py
+++ main.py
@@ -6,7 +6,9 @@ from interpreter import FtpInterpreter
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--debug', action='store_true')
+    parser.add_argument('--debug', action='store_true',
+                        help='Use this to see debug output from the '
+                             'FTP client.')
     args = parser.parse_args(sys.argv[1:])
 
     ftps_interpreter = FtpInterpreter(debug=args.debug)
